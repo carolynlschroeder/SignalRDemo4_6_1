72e8b4429fd84a01d827847478b6eac715cf92d1
Carolyn <carolynlschroeder@hotmail.com>
2016-06-18 16:10:25 -0500

    Modified code to show/hide likes.  Added image for like anchor

--- SignalRDemo4_6_1/Scripts/likes.js
+++ SignalRDemo4_6_1/Scripts/likes.js
@@ -1,32 +1,23 @@
 ﻿$(function () {
 
-    $(".like")
-        .each(function() {
-            var hiddenlike = $(this).siblings("input.userLikes:hidden").val();
-            if (hiddenlike == "False") {
-                $(this).css("display","");
-                $(this).siblings("span.liked").css("display", "none");
-            } else {
-                $(this).css("display", "none");
-                $(this).siblings("span.liked").css("display", "");
-            }
-        });
-
     var demoHub = $.connection.signalRDemoHub;
     demoHub.client.addNewMessageToPage = function (userId, imageId) {
         var count = parseInt($("#tLikes_" + imageId).text());
         var txt = (count + 1) + " Likes";
         $("#tLikes_" + imageId).text(txt);
         if ($("#userId").val() == userId) {
-            $("#a_" + imageId).css("display", "none");
-            $("#sp_" + imageId).css("display", "");
+            $("#a_" + imageId).removeClass("visible");
+            $("#a_" + imageId).addClass("notvisible");
+            $("#sp_" + imageId).removeClass("notvisible");
+            $("#sp_" + imageId).addClass("visible");
         }
     }
 
     $.connection.hub.start()
        .done(function () {
            $('.like')
-               .click(function () {
+               .click(function (e) {
+                   e.preventDefault();
                    var userId = $("#userId").val();
                    var imageId = $(this).siblings("input.imageId:hidden").val();
                    demoHub.server.likeImage(userId, imageId);
